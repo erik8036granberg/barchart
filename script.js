560ce81cfe54e62cc313a7e4734721fc616b3b29
Erik Granberg <36399140+erik8036granberg@users.noreply.github.com>
2019-02-14 17:55:12 +0100

    With columns set to 35 max & colored columns

--- script.js
+++ script.js
@@ -16,36 +16,53 @@ function createBoxes() {
   console.log("counter");
 
   // add number to array
-  counter.unshift(number.toFixed(0));
+  counter.unshift(number);
   console.log(counter);
 
-  const clone = document
-    .querySelector("#boxtemplate")
-    .content.cloneNode(true);
-
+  // clone box
+  const clone = document.querySelector("#boxtemplate").content.cloneNode(true);
   const box = clone.querySelector("#box");
+  const cloneBox = document.querySelector("#container").append(clone);
 
-  let height = getRandomNumber() + "px";
-  console.log(height);
-  box.style.height = getRandomNumber() + "px";
+  // random number
+  let randomNumber = getRandomInt(0, 35);
+  console.log(randomNumber);
 
-  const elm = document.querySelector("#container").append(clone);
+  // box height & color
+  box.style.height = randomNumber + "vh";
+
+  if (randomNumber >= 0 && randomNumber <= 5) {
+    box.style.backgroundColor = "Red";
+  }
+  if (randomNumber >= 6 && randomNumber <= 10) {
+    box.style.backgroundColor = "Orange";
+  }
+  if (randomNumber >= 11 && randomNumber <= 19) {
+    box.style.backgroundColor = "Yellow";
+  }
+  if (randomNumber >= 20 && randomNumber <= 29) {
+    box.style.backgroundColor = "LimeGreen";
+  }
+  if (randomNumber > 30) {
+    box.style.backgroundColor = "Green";
+  }
 
   // counter
   number++;
 
   // limit to - delete the last
   if (counter.length > max_length) {
-
     //Remove the first one
     document.querySelector("#box").remove();
+    counter.shift(number);
   }
 
   // timer function
   setTimeout(createBoxes, 1000);
-};
-
-function getRandomNumber() {
-  const num = Math.random() * 100;
-  return num;
+}
+//Random function - The maximum is exclusive and the minimum is inclusive
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
